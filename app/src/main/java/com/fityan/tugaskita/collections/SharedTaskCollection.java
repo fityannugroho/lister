605e6153fd9f822103155a0179ba20285032496a
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-01-04 21:06:30 +0700

    remove unused import

--- app/src/main/java/com/fityan/tugaskita/collections/SharedTaskCollection.java
+++ app/src/main/java/com/fityan/tugaskita/collections/SharedTaskCollection.java
@@ -5,12 +5,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SharedTaskCollection {
 
