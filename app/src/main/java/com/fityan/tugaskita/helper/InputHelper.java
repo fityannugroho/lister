1cf682dcbff0478b730ae624f9c97087571ce1e5
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-01-04 21:01:24 +0700

    update date-string converter

--- app/src/main/java/com/fityan/tugaskita/helper/InputHelper.java
+++ app/src/main/java/com/fityan/tugaskita/helper/InputHelper.java
@@ -10,6 +10,9 @@ import java.util.Date;
 import java.util.Locale;
 
 public class InputHelper {
+  public static final String DATE_FORMAT_HUMAN_US = "EE, MMM dd 'at' hh.mm a";
+
+
   /**
    * Get required input value.
    *
@@ -31,25 +34,51 @@ public class InputHelper {
 
 
   /**
-   * Convert date to string by following "yyyy/MM/dd HH:mm" format.
+   * Convert date to string to human readable format (US).
    *
    * @param date The date.
    * @return The formatted date in string.
    */
   @NonNull
   public static String dateToString(Date date) {
-    return new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.US).format(date);
+    return new SimpleDateFormat(DATE_FORMAT_HUMAN_US, Locale.US).format(date);
   }
 
 
   /**
-   * Convert string that following "yyyy/MM/dd HH:mm" format to date.
+   * Convert date to string by following specified format.
+   *
+   * @param date   The date.
+   * @param format The specified date format.
+   * @return The formatted date in string.
+   */
+  @NonNull
+  public static String dateToString(Date date, String format) {
+    return new SimpleDateFormat(format, Locale.ROOT).format(date);
+  }
+
+
+  /**
+   * Convert date from human readable format (US) string date.
    *
    * @param strDate The suitable string date.
    * @return The date.
    * @throws ParseException If string doesn't match the format.
    */
   public static Date stringToDate(String strDate) throws ParseException {
-    return new SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.US).parse(strDate);
+    return new SimpleDateFormat(DATE_FORMAT_HUMAN_US, Locale.US).parse(strDate);
+  }
+
+
+  /**
+   * Convert date from specified format string date.
+   *
+   * @param strDate The suitable string date.
+   * @param format  The specified date format.
+   * @return The date.
+   * @throws ParseException If string doesn't match the format.
+   */
+  public static Date stringToDate(String strDate, String format) throws ParseException {
+    return new SimpleDateFormat(format, Locale.ROOT).parse(strDate);
   }
 }
