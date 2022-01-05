a4c5a8f89265ee7f50b7fb9cdcf3fbd767ef46e5
Fityandhiya Islam Nugroho <fityandhiya@gmail.com>
2022-01-06 06:52:54 +0700

    added share task dialog

--- app/src/main/java/com/fityan/tugaskita/activities/DetailTaskActivity.java
+++ app/src/main/java/com/fityan/tugaskita/activities/DetailTaskActivity.java
@@ -1,20 +1,28 @@
 package com.fityan.tugaskita.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fityan.tugaskita.R;
 import com.fityan.tugaskita.collections.TaskCollection;
+import com.fityan.tugaskita.components.ShareTaskDialog;
 import com.fityan.tugaskita.helper.InputHelper;
 import com.fityan.tugaskita.models.TaskModel;
 import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Locale;
 import java.util.Objects;
 
-public class DetailTaskActivity extends AppCompatActivity {
+public class DetailTaskActivity extends AppCompatActivity implements ShareTaskDialog.OnClickShareTaskDialogListener {
   /* Collections */
   private final TaskCollection taskCollection = new TaskCollection();
   /* View elements. */
@@ -49,5 +57,32 @@ public class DetailTaskActivity extends AppCompatActivity {
           tvDescription.setText(documentSnapshot.getString(TaskModel.DESCRIPTION_FIELD));
           tvDeadline.setText(strDeadline);
         });
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.detail_task_menu, menu);
+    return true;
+  }
+
+
+  @Override
+  public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+    /* If Share Item is selected. */
+    if (item.getItemId() == R.id.shareItem) {
+      /* Show share task dialog. */
+      ShareTaskDialog shareTaskDialog = new ShareTaskDialog();
+      shareTaskDialog.show(getSupportFragmentManager(), "shareTaskDialog");
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+
+  @Override
+  public void onClickShareButton(String email, boolean isWritable, boolean isDeletable) {
+    /* TODO: Check if email is exists. */
+    Toast.makeText(this, "Email: " + email, Toast.LENGTH_SHORT).show();
   }
 }
